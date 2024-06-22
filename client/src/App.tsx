1903d8419e49082d3f328172f0493951a2569b66
Gabriel-Sison <gabrielarceo0183@gmail.com>
2024-06-21 20:33:09 -0700

    Fixed day error from server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import { Food } from "./Food";
 import { Calculator } from "./Calculator";
-import { deleteFood, listFoods, saveFood} from "./server";
+import { deleteFood, listFoods, saveFood, getDay, updateDay} from "./server";
 import { NewFood } from "./NewFood";
 
 type Page = "Inventory" | "Calculator" | "New Food"
 type AppState = {
-    page: Page, foodList: Array<Food>
+    page: Page, foodList: Array<Food>, day: number
 }
 
 export class App extends Component<{}, AppState> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            page: "Calculator", foodList: []
+            page: "Calculator", foodList: [], day: 0
         };
     }
 
     componentDidMount(): void {
-        this.doGetList();
+        listFoods(this.doListResp)
+        getDay(this.doGetDayResp)
     }
 
     render = (): JSX.Element => {
@@ -26,6 +27,8 @@ export class App extends Component<{}, AppState> {
             return <Calculator  onDeleteClick={this.doDeleteClick} 
                                 onAddClick={this.doAddClick} 
                                 foods={this.state.foodList}
+                                day={this.state.day}
+                                onDayChange={this.doDayChange}
                                 onAddFoodPage={this.doAddFoodPageChange}/>
         } else if (this.state.page === "Inventory") {
             return <div>Trying to go to inventory</div>
@@ -33,10 +36,6 @@ export class App extends Component<{}, AppState> {
             return <NewFood onAddClick={this.doAddClick}></NewFood>
         }
     };
-
-    doGetList = (): void => {
-        listFoods(this.doListResp)
-    }
 
     doAddFoodPageChange = (): void => {
         this.setState({page: "New Food"});
@@ -48,19 +47,21 @@ export class App extends Component<{}, AppState> {
 
     doAddClick = (food: Food): void => {
         saveFood(food)
-        this.doGetList()
+        listFoods(this.doListResp)
         this.setState({page: "Calculator"})
     }
 
     doDeleteClick = (food: Food): void => {
         deleteFood(food)
+        listFoods(this.doListResp)
     }
 
-    // doDayChange = (day: number): void => {
-    //     updateDay(day)
-    // }
+    doDayChange = (day: number): void => {
+        updateDay(day)
+        this.setState({day: day})
+    }
 
-    // doGetDayResp = (day: number): void => {
-    //     this.setState({day: day})
-    // }
+    doGetDayResp = (serverDay: number): void => {
+        this.setState({day: serverDay})
+    }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { addFood, listFoods, deleteFood} from './routes';
+import { addFood, listFoods, deleteFood, updateDay, getDay} from './routes';
 import bodyParser from 'body-parser';
 
 // Configure and start the HTTP server.
@@ -8,10 +8,7 @@ const app: Express = express();
 app.use(bodyParser.json());
 app.post("/api/add", addFood)
 app.post("/api/delete", deleteFood)
-
-// For some reason it's causing an error connecting to server
-// app.post("/api/updateDay", updateDay)
-// app.get("/api/getDay", getDay)
-
+app.post("/api/updateDay", updateDay)
+app.get("/api/getDay", getDay)
 app.get("/api/list", listFoods)
 app.listen(port, () => console.log(`Server listening on ${port}`));
